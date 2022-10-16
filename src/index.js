1f3207bfd5ef0e50eec3dd0b3d206aa202fcc0e7
NenevAleksey <av.nenev@gmail.com>
2022-10-16 19:02:28 +0600

    получение данных по ipa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('.search-bar__btn')
+
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+   //проверка данных
+   fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_pDzZQgOH7MfWwtJaGqBDa3vVKtoAl&ipAddress=${ipInput.value}`)
+      .then(response => response.json())
+      .then(console.log)
+}
+
+function handleKey(e) {
+   if (e.key === 'Enter') {
+      getData();
+   }
+}
