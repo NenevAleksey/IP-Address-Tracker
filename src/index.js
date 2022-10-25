b2206317da4af285bcf609300df3d57773f10293
NenevAleksey <av.nenev@gmail.com>
2022-10-25 19:08:07 +0600

    добавление карты и маркера

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import {validateIp} from './helpers';
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet';
+import { validateIp } from './helpers';
+import icon from '../images/icon-location.svg'
 
 //переменные
 const ipInput = document.querySelector('.search-bar__input');
@@ -13,14 +16,32 @@ const ispInfo = document.getElementById('isp');
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
+//карта
+const mapArea = document.querySelector('.map');
+const markerIcon = L.icon({
+   iconUrl: icon,
+   iconSize: [30, 40],
+});
+const map = L.map(mapArea, {
+   center: [51.505, -0.09],
+   zoom: 13,
+   zoomControl: false,
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   maxZoom: 19,
+   attribution: 'Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="#">Aleksey Nenev</a>.'
+}).addTo(map);
+L.marker([51.505, -0.09], {icon: markerIcon}).addTo(map)
+
 //получение данных
 function getData() {
    //проверка данных
-   if(validateIp(ipInput.value)) {
-      
+   if (validateIp(ipInput.value)) {
+
       fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_pDzZQgOH7MfWwtJaGqBDa3vVKtoAl&ipAddress=${ipInput.value}`)
-      .then(response => response.json())
-      .then(data => setInfo(data))
+         .then(response => response.json())
+         .then(data => setInfo(data))
    }
 }
 function handleKey(e) {
